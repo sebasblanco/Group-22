bdf82875c54d587908f5d52786384e46f084ea26
JDWhiddon <103385160+JDWhiddon@users.noreply.github.com>
2024-02-04 20:40:34 -0500

    Added azure functions, as well as support for getting LoL puuid from username and tag

--- react-app/src/components/LeagueOfLegendsStats.jsx
+++ react-app/src/components/LeagueOfLegendsStats.jsx
@@ -1,42 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-// This key is automatically going to reset at 11:13AM on Feb 5th, so it wont work after that until John refreshses the key
-const API_KEY="RGAPI-e798d195-1699-4872-be89-25e47e7ac361"
 
-const LeagueOfLegendsStats = ({username, tag}) => {
+const LeagueOfLegendsStats = ({ username, tag }) => {
+    const [puuid, setPuuid] = useState(null);
 
-    var APICallString = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + username + "/" + tag + "?api_key=" + API_KEY;
-        console.log(APICallString)
-        axios.get(APICallString).then(function (response) {
-            //Success
-            console.log(response)
-        
-        }).catch(function (error) {
-            console.log(response)
-            //Error
-        })
-    
-    
-    function searchForPlayer(event) {
-        axios.get(APICallString).then(function (response) {
-            //Success
-            console.log(response)
-        
-        }).catch(function (error) {
-            console.log(response)
-            //Error
-        })
-    }
+    useEffect(() => {
+        const fetchData = () => {
+            var APICallString = "http://localhost:7071/api/leaguegetpuuid?username=" + username + "&tag=" + tag;
+            console.log(APICallString);
 
+            axios.get(APICallString)
+                .then(function (response) {
+                    // Success
+                    console.log(response.data.puuid);
+                    setPuuid(response.data.puuid);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                    // Error
+                });
+        };
 
-    return (<div>
-        LoL stats for {username}
-        {APICallString}
-    </div>
+        fetchData();
+    }, [username, tag]);
 
-
-
-
+    return (
+        <div>
+            <p>LoL stats for {username}</p>
+            {puuid && (
+                <p>Their puuid is {puuid}</p>
+            )}
+        </div>
     );
 }
 
