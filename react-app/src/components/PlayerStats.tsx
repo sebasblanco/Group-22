eae6f5db4bc8c580da64a1dfa206c83598027b55
Nicholas Bosque <nathanielnb@gmail.com>
2024-02-21 11:11:37 -0500

    Adjusted PlayerStats.tsx to include World of Warcraft - adds functions to show World of Warcraft relevant elements

--- react-app/src/components/PlayerStats.tsx
+++ react-app/src/components/PlayerStats.tsx
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 import LeagueOfLegendsStats from "./league-components/LeagueOfLegendsStats";
 import OWLStats from "./OWL-components/OWLStats";
+import WoWStats from "./WoW-components/WoWStats";
 
 function HtmlForm() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const [username, setUsername] = useState<string>("");
   const [gameStats, setGameStats] = useState<JSX.Element | null>(null);
+  const [realm, setRealm] = useState<string>("");
 
   const handleItemClick = (item: string) => {
-    setSelectedItem(item);
-  };
+      setSelectedItem(item);
+    };
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
-  };
+    };
+
+  const handleRealmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRealm(event.target.value);
+    };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevent the default form submission
     // Handle the selected item and username
     console.log("Selected game:", selectedItem);
     console.log("Username:", username);
+
     // Load game stats only if the username and game are selected
     if (selectedItem && username) {
       // Load game stats based on selectedItem and username
@@ -29,8 +36,11 @@ function HtmlForm() {
       } 
       else if (selectedItem === "Overwatch") {
         const [gameName, tag] = username.split("#");
-        setGameStats(<OWLStats username={gameName} tag={tag} />);
-      } 
+          setGameStats(<OWLStats username={gameName} tag={tag} />);
+      }
+      else if (selectedItem === "World of Warcraft") {
+          setGameStats(<WoWStats charactername={username} realm={realm} />);
+      }
       else {
         // Handle other games here if needed
         setGameStats(null); // Clear game stats if not applicable
@@ -38,7 +48,7 @@ function HtmlForm() {
     }
   };
 
-  return (
+    return (
     <>
       <div>
         <h1>GamerInsights</h1>
@@ -75,13 +85,31 @@ function HtmlForm() {
                   League of Legends
                 </a>
               </li>
+              <li>
+                <a
+                   className="dropdown-item"
+                   href="#"
+                   onClick={() => handleItemClick("World of Warcraft")}
+                >
+                   World of Warcraft
+                </a>
+              </li>
             </ul>
           </div>
           {/* End of dropdown menu */}
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="htmlForm-label">
-              Username
-            </label>
+            {(function () {
+                if (selectedItem === "World of Warcraft") {
+                    return <label htmlFor="exampleInputPassword1" className="htmlForm-label">
+                                Character Name
+                            </label>;
+                }
+                else {
+                    return <label htmlFor="exampleInputPassword1" className="htmlForm-label">
+                                Username
+                            </label>;
+                }
+            })()}
             <br />
             <input
               type="text"
@@ -91,6 +119,26 @@ function HtmlForm() {
               onChange={handleUsernameChange}
             />
           </div>
+            {/* Conditional World of Warcraft label to get realm name */}
+            {(function () {
+                if (selectedItem === "World of Warcraft") {
+                    return <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="htmlForm-label">
+                            Realm
+                        </label>
+                        <br />
+                        <input
+                            type="text"
+                            className="htmlForm-control"
+                            id="realm"
+                            value={realm}
+                            onChange={handleRealmChange}
+                        />
+                    </div>;
+                } else {
+                    return null;
+                }
+            })()}       
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
