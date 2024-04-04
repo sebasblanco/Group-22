70e85c619c1d0464b306d65167ec4863407248f0
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2024-04-04 00:38:52 -0400

    Refactor metadata in Features component

--- gamerinsights/src/app/features/page.tsx
+++ gamerinsights/src/app/features/page.tsx
@@ -3,12 +3,12 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 
 export const metadata: Metadata = {
-  title: "Features",
-  description: "Gamer Insights Features",
+    title: "Features",
+    description: "Gamer Insights Features",
 };
 
 export default function Features() {
-    return(
+    return (
         <div>
             <Header />
             <h1>Features</h1>
