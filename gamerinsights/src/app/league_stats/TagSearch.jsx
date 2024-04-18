85e95b2c3eae6d959df70d43969e639808aef55f
JDWhiddon <103385160+JDWhiddon@users.noreply.github.com>
2024-04-18 18:55:25 -0400

    Added search functionality to league stats

---
+++ gamerinsights/src/app/league_stats/TagSearch.jsx
@@ -0,0 +1,7 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./components/LeagueOfLegendsStyle.css";
+import LoLMasteryStats from "./components/LoLMasteryStats";
+import LoLMatchHistory from "./components/LoLMatchHistory";
+import LeagueOfLegendsStats from "./components/LeagueOfLegendsStats";
--- gamerinsights/src/app/league_stats/page.tsx
+++ gamerinsights/src/app/league_stats/page.tsx
@@ -1,12 +1,37 @@
+"use client";
+import React, { useState } from "react";
 import LeagueOfLegendsStats from "./LeagueOfLegendsStats";
 
 export default function Features() {
+  const [usernameAndTag, setUsernameAndTag] = useState("DoubleLift#NA1");
+
+  const handleInputChange = (event) => {
+    setUsernameAndTag(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Username and tag: ", usernameAndTag);
+  };
+
   return (
     <div>
-      <LeagueOfLegendsStats
-        username={"Doublelift"}
-        tag={"na1"}
-      ></LeagueOfLegendsStats>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter username#tag"
+          value={usernameAndTag}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {usernameAndTag && (
+        <LeagueOfLegendsStats
+          username={usernameAndTag.split("#")[0]}
+          tag={usernameAndTag.split("#")[1]}
+        />
+      )}
     </div>
   );
 }
