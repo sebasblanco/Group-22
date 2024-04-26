0bbdf625fa6a3804299a26ef735622f89908c885
JDWhiddon <103385160+JDWhiddon@users.noreply.github.com>
2024-04-26 19:45:02 -0400

    Fixed font color on nav bars

--- gamerinsights/src/app/dashboard/components/SignOut.tsx
+++ gamerinsights/src/app/dashboard/components/SignOut.tsx
@@ -1,13 +1,19 @@
-'use client'
+"use client";
 
-import { DropdownMenuItem, DropdownMenuShortcut } from "@/components/ui/dropdown-menu";
-import { signOut } from "next-auth/react"
+import {
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+} from "@/components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
 
 export default function SignOutButton() {
-    return (
-        <DropdownMenuItem onClick={() => signOut({ callbackUrl: '/' })}>
-            Log out
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-    );
+  return (
+    <DropdownMenuItem
+      onClick={() => signOut({ callbackUrl: "/" })}
+      className="text-white"
+    >
+      Log out
+      <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+    </DropdownMenuItem>
+  );
 }
