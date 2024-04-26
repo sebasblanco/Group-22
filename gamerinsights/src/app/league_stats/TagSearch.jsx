dfed1c82bb4d20bced59bd2ff20266a398c2edf4
JDWhiddon <103385160+JDWhiddon@users.noreply.github.com>
2024-04-26 18:56:53 -0400

    Deleted old league_stats

--- gamerinsights/src/app/league_stats/TagSearch.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./components/LeagueOfLegendsStyle.css";
-import LoLMasteryStats from "./components/LoLMasteryStats";
-import LoLMatchHistory from "./components/LoLMatchHistory";
-import LeagueOfLegendsStats from "./components/LeagueOfLegendsStats";
