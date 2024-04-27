35fdc73dbbdf0f16c647fcb272611c98eb8ad31d
JDWhiddon <103385160+JDWhiddon@users.noreply.github.com>
2024-04-26 21:22:23 -0400

    Fixed league icon margin

--- gamerinsights/src/app/dashboard/components/league_icon.tsx
+++ gamerinsights/src/app/dashboard/components/league_icon.tsx
@@ -6,7 +6,7 @@ const LeagueIcon: React.FC = () => {
       style={{
         height: "40px",
         width: "auto",
-        marginBottom: "25px",
+        // marginBottom: "25px",
       }}
       src="/league_icon_white.png"
       alt="League Icon"
